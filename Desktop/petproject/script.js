a1a751671c286ce1834ba155d917e88679a8c866
khan sabiya <khan901145@gmail.com>
2026-07-17 07:37:49 +0530

    added new files

---
+++ Desktop/petproject/script.js
@@ -0,0 +1,77 @@
+// let todo =[];
+//  let req = prompt("please enter your request :");
+
+//  while(true){
+//    if(req =="quit"){
+//       console.log("quitting app");
+//       break;
+//    }
+//    if(req == "list"){
+//       console.log("______________");
+
+//       for(let i=0;i<todo.length;i++){
+//          console.log(i,todo[i]);
+//       }
+//       console.log("_______________");
+//    }
+//    else if(req == "add"){
+//       let task = prompt("please enter a task you want to add");
+//       todo.push (task);
+//       console.log ("task added");
+//    } 
+//    else if (req =="delete"){
+//       let idx = prompt("please enter the task index");
+//       todo.splice(idx,1);
+//       console.log ("task deleted")
+//    }else{
+//       console.log("wrong request");
+      
+//    }
+//    req = prompt("please enter your request");
+//  }
+
+
+
+
+// const max = prompt("enter the max number");
+// const random = Math.floor(Math.random()*max)+1;
+// let guess = prompt("guess the number");
+// while(true){
+//    if(guess=="quit"){
+//       console.log("user quit");
+//       break;
+//    }
+//    if(guess == random){
+//       console.log("your guess was right,congrats!!!");
+//       break;
+//    }
+//    else if(guess<random){
+//       guess=prompt("your guess was too samll.please try again");
+
+//    }else{
+//       guess = prompt("your guess was too large.please try again");
+//    }
+//}
+// function oddorevenfactory(request){
+//    if( request=="odd"){
+//       let odd = function(n){
+//          c
+//       }
+//    }
+// }
+
+// --------------------- color generator----------------
+
+// let url="https://icanhazdadjoke.com/";
+
+// async function getjokes(){
+//   try{
+//     const config ={ headers :{Accept:"appliction/json"}};
+//     let res = await axios.get(url,config);
+//    console.log(res.data);
+   
+//   }catch(err){
+//     console.log(err);
+//   }
+// }
+ 
